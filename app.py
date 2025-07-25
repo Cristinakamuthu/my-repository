33b8406a7a966869067730a2ebc07f834fb9a5a5
Cristina <kamuthucristina@gmail.com>
2025-07-25 08:07:21 +0300

    adjusted the front end link

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 CORS(app,
      origins=[
          "http://localhost:3000",
-         "https://moringa-lost-and-found-frontend.vercel.app"
+         "https://lost-now-found.vercel.app"
      ],
      supports_credentials=True)
 
